dd48ae1a3fe8835a3bdd5d8c15c5dfc24835e8f6
NiniolaX <loveafinni16@gmail.com>
2024-05-28 15:10:28 +0100

    Removed debug=True in app config

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,7 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+# Register Blueprint
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
@@ -34,6 +35,6 @@ def return_404(error):
 
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, threaded=True, debug=True)
+    hostname = getenv('HBNB_API_HOST', '0.0.0.0')
+    portname = getenv('HBNB_API_PORT', 5000)
+    app.run(host=hostname, port=portname, threaded=True)
