85f4e011254a7c6aa07ca73ba98370a2bc3ebbad
NiniolaX <loveafinni16@gmail.com>
2024-05-28 15:17:54 +0100

    Updated 404 error handler to return 404 error code

--- api/v1/app.py
+++ api/v1/app.py
@@ -31,7 +31,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def return_404(error):
     """ Returns a 404 error message in JSON """
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
